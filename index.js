4c5b3dda6d73ffd62d9c7752faef640912fb3f9b
ozairkhan2810 <ozairkhan2810@gmail.com>
2021-09-21 08:32:59 +0530

    update style

--- index.js
+++ index.js
@@ -4,25 +4,25 @@ let translateOutput = document.querySelector("#translate-output")
 
 const SERVER_URL = "https://api.funtranslations.com/translate/groot.json";
 
-translateButton.addEventListener("click", fetchTranslated)
+translateButton.addEventListener("click", fetchTranslated);
 
 function fetchTranslated() {
-    let inputText = translateInput.value
+    let inputText = translateInput.value;
     if (inputText == "") {
         alert("Please enter some text");
         return;
     }
 
-    const url = SERVER_URL + "?" + "text=" + inputText
+    const url = SERVER_URL + "?" + "text=" + inputText;
     fetch(url)
         .then(res => res.json())
         .then(data => {
             console.log(data)
-            var translatedText = data.contents.translated
-            translateOutput.innerText = translatedText
+            var translatedText = data.contents.translated;
+            translateOutput.innerText = translatedText;
         })
         .catch(err => {
-            console.log(err)
-            alert("Not response from server!")
+            console.log(err);
+            alert("Not response from server!");
         })
 }
